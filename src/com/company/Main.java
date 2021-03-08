b7213641adb25ec0b91341da9ce79153a32c1988
LeviMatias <matias.levi@mercadolibre.com>
2021-03-08 12:39:45 -0300

    adding code wih me files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,13 +14,13 @@ public class Main {
 //        arr[4] =  new Persona(84, "Matias");
 //        arr[5] =  new Persona(132, "Lucas");
 //        arr[6] = new Persona(45, "Javier");
-        arr[0] = new Celular("Rodrigo");
-        arr[1] =  new Celular("Juan");
-        arr[2] =  new Celular("Mateo");
-        arr[3] =  new Celular(10, "Pedro");
-        arr[4] =  new Celular(84, "Matias");
-        arr[5] =  new Celular(132, "Lucas");
-        arr[6] = new Celular(45, "Javier");
+        arr[0] = new Celular("Rodrigo", 423421);
+        arr[1] =  new Celular("Juan", 1545452);
+        arr[2] =  new Celular("Mateo",5225435);
+        arr[3] =  new Celular("Pedro", 542425);
+        arr[4] =  new Celular("Matias", 54254525);
+        arr[5] =  new Celular("Lucas", 545254);
+        arr[6] = new Celular("Javier",545444);
 
         SortUtil.ordenar(arr);
 
